480eafb76133cd618376805cdb6f346766ccc05e
Niqtan <niqban123@gmail.com>
2025-01-20 17:11:13 +0800

    push everything

--- ch6/exercises/6.1.c
+++ ch6/exercises/6.1.c
@@ -4,37 +4,48 @@
 /* getword: get next word or character from input */
 
 #include <stdio.h>
+#include <string.h>
 #include <ctype.h>
 
 int getword(char *word, int lim)
 {
-    int c, getch(void);
+    int i, c, getch(void);
     void ungetch(int);
     char *w = word;
+    char arr[20];
 
     while (isspace(c = getch()))
     ;
     if (c != EOF)
         *w++ = c;
-    if (!isalpha(c) || c != '_') {
+    
+    if (c == '_') {
+        *w++ = c;
+    }
+    else if (c == '"') {
+        while (c = getch() && c != '"') {
+            *w++ = c;
+        }
+        *w++ = '\0';
+    }
+    else if(c == '/') { 
+        if (c == '/') {
+            //skip everything
+        }
+    }
+    //For directive
+    else {
+        while (c = getch() && c != '\0') {
+            *w++ = c;
+        }
+        *w++ = '\0';
+    }
+    
+
+    if (!isalpha(c)) {
         *w = '\0';
         return c;
     }
-
-    /*
-    Use a switch case statement to check for each pattern
-    
-    */
-   switch(c) {
-    case '_':
-        //Do something
-    case '#':
-        //do something
-    case '""':
-        //Do something with string constants
-    case '//':
-        //do something
-   }
 
     for ( ; --lim > 0; w++)
         if (!isalnum(*w = getch())) {
